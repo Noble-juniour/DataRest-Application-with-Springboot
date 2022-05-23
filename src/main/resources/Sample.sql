2c444a91d02a691484f1fd742e5a34043616581f
kevin <kevinindangasi1@gmail.com>
2022-05-23 15:48:21 +0300

    Data Rest API with Springboot

---
+++ src/main/resources/Sample.sql
@@ -0,0 +1,4 @@
+INSERT INTO VALUES ('379' , 'Sahiti1' ,'computer science');
+INSERT INTO VALUES ('3795' , 'Sahiti2' ,'math and computer science');
+INSERT INTO VALUES ('37953' , 'Sahiti3' ,'History');
+INSERT INTO VALUES ('379533' , 'Sahiti4' ,'kiswahili');
